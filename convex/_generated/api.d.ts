483196bd3344fe2fc48dfc4f0d5f5d838f17833c
Azhar Ahmad <ahmadao2214@gmail.com>
2025-12-29 14:29:11 -0800

    update types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,6 +10,7 @@
 
 import type * as exercises from "../exercises.js";
 import type * as gppWorkoutSessions from "../gppWorkoutSessions.js";
+import type * as intensityScaling from "../intensityScaling.js";
 import type * as invitations from "../invitations.js";
 import type * as programTemplates from "../programTemplates.js";
 import type * as sampleData from "../sampleData.js";
@@ -17,6 +18,7 @@ import type * as scheduleOverrides from "../scheduleOverrides.js";
 import type * as seed from "../seed.js";
 import type * as seedData from "../seedData.js";
 import type * as sports from "../sports.js";
+import type * as userMaxes from "../userMaxes.js";
 import type * as userPrograms from "../userPrograms.js";
 import type * as users from "../users.js";
 import type * as workoutSessions from "../workoutSessions.js";
@@ -31,6 +33,7 @@ import type {
 declare const fullApi: ApiFromModules<{
   exercises: typeof exercises;
   gppWorkoutSessions: typeof gppWorkoutSessions;
+  intensityScaling: typeof intensityScaling;
   invitations: typeof invitations;
   programTemplates: typeof programTemplates;
   sampleData: typeof sampleData;
@@ -38,6 +41,7 @@ declare const fullApi: ApiFromModules<{
   seed: typeof seed;
   seedData: typeof seedData;
   sports: typeof sports;
+  userMaxes: typeof userMaxes;
   userPrograms: typeof userPrograms;
   users: typeof users;
   workoutSessions: typeof workoutSessions;
